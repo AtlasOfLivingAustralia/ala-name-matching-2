38b39e3366ba34309ec87661534b1a12a55da169
pal155 <Doug.Palmer@csiro.au>
2020-08-03 10:56:33 +1000

    Use a modified version of the current GBIF name parser.

--- ala-linnaean/src/main/java/au/org/ala/names/AlaNameAnalyser.java
+++ ala-linnaean/src/main/java/au/org/ala/names/AlaNameAnalyser.java
@@ -5,18 +5,15 @@ import au.org.ala.bayesian.EvidenceAnalyser;
 import au.org.ala.bayesian.InferenceException;
 import au.org.ala.bayesian.StoreException;
 import au.org.ala.util.SimpleClassifier;
-import org.gbif.api.exception.UnparsableException;
-import org.gbif.api.model.checklistbank.ParsedName;
-import org.gbif.api.vocabulary.Rank;
-import org.gbif.nameparser.PhraseNameParser;
+import com.google.common.base.Enums;
+import org.gbif.nameparser.NameParserGBIF;
+import org.gbif.nameparser.api.NameParser;
+import org.gbif.nameparser.api.ParsedName;
+import org.gbif.nameparser.api.Rank;
+import org.gbif.nameparser.api.UnparsableNameException;
 
 public class AlaNameAnalyser implements EvidenceAnalyser<AlaLinnaeanClassification> {
-    private static ThreadLocal<PhraseNameParser> PARSER = new ThreadLocal<PhraseNameParser>() {
-      @Override
-        protected PhraseNameParser initialValue() {
-            return new PhraseNameParser();
-        }
-    };
+    private static final ThreadLocal<NameParser> PARSER = ThreadLocal.withInitial(NameParserGBIF::new);
 
     /**
      * Analyse the information in a classifier and extend the classifier
@@ -24,35 +21,31 @@ public class AlaNameAnalyser implements EvidenceAnalyser<AlaLinnaeanClassificati
      *
      * @param classifier The classifier
      *
-     * @throws InferenceException if an error occurs during inference
      * @throws StoreException     if an error occurs updating the classifier
      */
     @Override
-    public void analyse(Classifier classifier) throws InferenceException, StoreException {
-        PhraseNameParser parser = PARSER.get();
+    public void analyse(Classifier classifier) throws StoreException {
+        NameParser parser = PARSER.get();
         String scientificName = classifier.get(AlaLinnaeanFactory.scientificName);
         String taxonRank = classifier.get(AlaLinnaeanFactory.taxonRank);
-        Rank rank = null;
+        Rank rank = taxonRank == null ? Rank.UNRANKED : Enums.getIfPresent(Rank.class, taxonRank).or(Rank.UNRANKED);
         try {
-            if (taxonRank != null)
-                rank = Rank.valueOf(taxonRank);
-        } catch (IllegalArgumentException ex) {
-        }
-        try {
-            ParsedName name = parser.parse(scientificName, rank);
+            ParsedName name = parser.parse(scientificName, rank, null);
             rank = name.getRank();
-            if (rank != null && Rank.SPECIES.higherThan(rank)) {
-                rank = Rank.INFRASPECIFIC_NAME;
-                classifier.replace(AlaLinnaeanFactory.taxonRank, rank.name());
-            } else if (!classifier.has(AlaLinnaeanFactory.taxonRank) && rank != null)
-                classifier.add(AlaLinnaeanFactory.taxonRank, rank.name());
+            if (rank != null && rank != Rank.UNRANKED) {
+                if (Rank.SPECIES.higherThan(rank)) {
+                    rank = Rank.INFRASPECIFIC_NAME;
+                    classifier.replace(AlaLinnaeanFactory.taxonRank, rank.name());
+                } else if (!classifier.has(AlaLinnaeanFactory.taxonRank))
+                    classifier.add(AlaLinnaeanFactory.taxonRank, rank.name());
+            }
             if (!classifier.has(AlaLinnaeanFactory.specificEpithet) && name.getSpecificEpithet() != null)
                 classifier.add(AlaLinnaeanFactory.specificEpithet, name.getSpecificEpithet());
-            if (!classifier.has(AlaLinnaeanFactory.genus) && rank != null && !rank.higherThan(Rank.GENUS) && name.getGenusOrAbove() != null)
-                classifier.add(AlaLinnaeanFactory.genus, name.getGenusOrAbove());
-        } catch (UnparsableException ex) {
+            if (!classifier.has(AlaLinnaeanFactory.genus) && rank != null && !rank.higherThan(Rank.GENUS) && name.getGenus() != null)
+                classifier.add(AlaLinnaeanFactory.genus, name.getGenus());
+        } catch (UnparsableNameException e) {
+            // Ignore this, we'll just have to roll along with an unrecognisable name
         }
-
     }
 
     /**
