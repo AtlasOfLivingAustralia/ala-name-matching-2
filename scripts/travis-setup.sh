e4a257269944e0124b380311f5a90ba277c44a10
charvolant <doug@charvolant.org>
2024-10-11 09:37:56 +1100

    Travis build

---
+++ scripts/travis-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xv
+base=${INDEX_DIR:-/data/lucene}
+version=${INDEX_VERSION:-20230725-5}
+bucket=${DATA_BUCKET:-ala-nameindexes}
+if [ -z "${AWS_AP}" ]; then
+  access=""
+else
+  access=.ap-${AWS_AP}
+fi
+access=${AWS_AP:us-east-1}
+mkdir -p "$base"
+for f in linnaean vernacular location
+do
+  file="${f}-${version}.zip"
+  curl --output "${base}/${file}" "https://${bucket}.s3${access}.amazonaws.com/${version}/${file}"
+  unzip -d "${base}" "${base}/${file}"
+done
